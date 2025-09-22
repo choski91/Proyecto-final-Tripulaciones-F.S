8c09a36ef52439cdf27e52b3157fba703f800f4d
fernanbga <fernanburgos@icloud.com>
2025-09-22 23:16:48 +0200

    agregado header con estilos (botones no funcionan)

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <div className="header__icon">
+        <img src="/logotripulacion.jpeg" alt="Logo" />
+      </div>
+      <nav className="header__nav">
+        <a href="#" className="header__link">Dashboard</a>
+        <a href="#" className="header__link">Historial</a>
+        <a href="#" className="header__link">Registro</a>
+        <a href="#" className="header__link">Simulador</a>
+      </nav>
+      <button className="header__logout">Logout</button>
+    </header>
+  );
 };
 
 export default Header;
