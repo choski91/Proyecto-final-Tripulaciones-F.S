ac2d50a50255cc86827346bccf4b93d186baff2c
fernanbga <fernanburgos@icloud.com>
2025-09-23 23:52:39 +0200

    agregamos el filtro de la tabla

--- client/src/components/Main/AlertContainer/AlertList/AlertList.jsx
+++ client/src/components/Main/AlertContainer/AlertList/AlertList.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import AlertCard from "./AlertCard/AlertCard";
 
-export default function AlertList({ alerts }) {
+export default function AlertList() {
+  const [loading, setLoading] = useState(false);
+  const [simulateAttack, setSimulateAttack] = useState([]);
+
+  const handleClick = async () => {
+    setLoading(true);
+    try {
+      const data = await axios.get(
+        "https://desafio-4w98.onrender.com/random-alert"
+      );
+      setSimulateAttack(data.data.alerta);
+    } catch (err) {
+      console.error(err.response || err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <div className="alert-list">
-      {alerts.map((alert, idx) => (
-        <AlertCard key={idx} alert={alert} />
-      ))}
-    </div>
+    <>
+      <button onClick={handleClick} disabled={loading} className="btn">
+        {loading ? "Cargando…" : "Simular ataque"}
+      </button>
+      <div className="alert-list">
+        {simulateAttack.map((alert, idx) => (
+          <AlertCard key={idx} alert={alert} />
+        ))}
+      </div>
+    </>
   );
 }
