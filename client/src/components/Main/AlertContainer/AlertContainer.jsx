461fcb2ce13b832081085057d4cd86644444b0fe
jonha20 <jonathan812000@gmail.com>
2025-09-24 20:35:19 +0200

    fixx/url tabla

--- client/src/components/Main/AlertContainer/AlertContainer.jsx
+++ client/src/components/Main/AlertContainer/AlertContainer.jsx
@@ -23,7 +23,7 @@ export default function AlertContainer() {
   // Traer alertas del backend local
   useEffect(() => {
     const fetchBackendAlerts = async () => {
-      const apiUrlRender = "window._env_?.VITE_BACKEND_URL";
+      const apiUrlRender = window._env_?.VITE_BACKEND_URL;
       if (!apiUrlRender) {
         console.error("VITE_BACKEND_URL no está definido en window._env_");
         return;
